2f182296e8689f140e81e58c7d7df14e2a52ccdb
Stephanie Sison <sjsison27@icloud.com>
2022-11-08 18:19:04 -0800

    first test complete

--- looping.rb
+++ looping.rb
@@ -1,6 +1,24 @@
+require 'pry'
+
+# Start a countdown from 10 to 1, after 1, I want it to log "Happy New Year!"
 def happy_new_year
-  # your code here
+  counter = 10
+  while counter > 0
+    puts counter
+  counter -= 1
+  end
+  puts "Happy New Year!"
 end
+
+def example_code
+  n = 0
+  while n < 10
+    puts n
+    n += 1
+  end
+end
+
+binding.pry
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -14,6 +32,8 @@ def fizzbuzz(num)
     num
   end
 end
+
+
 
 def fizzbuzz_printer
   # your code here
